eafcd8d9922f7e5386b68589eb65709d2a7a39c8
G-yjz <87161942+G-yjz@users.noreply.github.com>
2022-04-02 16:21:42 +0800

    Add files via upload

--- ECurve.h
+++ ECurve.h
@@ -7,35 +7,49 @@
 using namespace std;
 
 struct point {
-	int x;
-	int y;
+	long int x;
+	long int y;
 };
 
+//求两数最大公因式
+int gcd(int x, int y)
+{
+	if (y == 0)
+		return x;
+	else
+		return gcd(y, x%y);
+}
+
+//求得分母逆元(扩展欧几里得)
+int get_inverse_element(int num, int p)
+{
+	for (int i = 1; i < p; i++)
+	{
+		if (((i*num) % p) == 1)
+			return i;
+	}
+	return -1;
+}
 
 class ECurve {
 public:
 	ECurve(int p1, int a1, int b1);
 
-	void showEpab();								//显示循环群元素
+	void showEpab();					//显示循环群元素
 
 	int unEqLamda(point P, point Q);	//计算P不等于Q时的Lamda
 
-	int equalLamda(point P);					//计算P等于Q时的Lamda
+	int equalLamda(point P);		    //计算P等于Q时的Lamda
 
-	point PplusQ(point P, point Q);  //计算P+Q
+	point PplusQ(point P, point Q);     //计算P+Q
 
-	point kP(int k,point P);                //计算kP 
+	point kP(int k,point P);            //计算kP 
 
-	int getOrd(point P);
+	int getOrd(point P);				//求该元素的阶
 
 
-	vector<int> vx;        //存储循环群所有元素的x值
-	vector<int> vy;        //存储循环群所有元素的y值
-
-	//point ECodePoint(int key,point A_Q,point B_Q);
-	//point DCodePoint(point m,int kb);
-	vector<int> EncodeMsg(int key, point P, point A_Q, point B_Q, string message);
-	string DcodeMsg(int key, point P, point A_Q, point B_Q, vector<int> secret);
+	vector<int> EncodeMsg(int key, point A_Q, point B_Q, string message);//对明文加密
+	string DcodeMsg(int key, vector<int> secret);                        //对密文解密
 
 private:
 	int p, a, b;
@@ -47,36 +61,57 @@ ECurve::ECurve(int p1, int a1, int b1) :p(p1), a(a1), b(b1) {}
 
 void ECurve::showEpab()
 {
-	int x, y, z;
-	for (x = 0; x < p; x++)
+	int n = 0;//存储该曲线循环群元素个数
+	unsigned long long z;
+
+	for (int x = 1; x < p; x++)
 	{
-		z = x * x * x + a * x + b;
+		z = pow(x,3)+ a * x + b;
 		if (z >= p)    //模p平方剩余
 		{
-			for (y = 0; y < p; y++)
-				if (((y*y) % p) == z % p)   //成立求得一个循环群元素
+			for (int y = 1; y < p; y++)
+			{
+				if (((y*y) % p) == (z % p))   //成立求得一个循环群元素
 				{
 					cout << "(" << x << "," << y << ") ";
-					vx.push_back(x);        //存储当前元素的x值
-					vy.push_back(y);        //存储当前元素的y值
-				}		
+					n += 1;
+				}
+			}	
 		}
 	}
-	cout << endl;
+	cout << endl << "该曲线循环群元素个数:" << n << endl;
 }
 
 int ECurve::unEqLamda(point P, point Q) {
 
-	int up = ((Q.y - P.y) % p + p) % p, down = ((Q.x - P.x) % p + p) % p;
-	int temp = pow(down, p - 2);
-	return ((up*temp) % p + p) % p;
+	long int up = ((Q.y - P.y) % p + p) % p, down = ((Q.x - P.x) % p + p) % p;//分子、分母
+
+	//将分子和分母化为最简
+	long int gcd_value = gcd(up, down);
+	up = int(up / gcd_value);
+	down = int(down / gcd_value);
+	
+	//求分母的逆元
+	long int inverse_value = get_inverse_element(down, p);
+	long long int lamda = (up*inverse_value);//求得lamda
+
+	return lamda;
 }
 
 int ECurve::equalLamda(point P) {
 
-	int up = ((3 * P.x * P.x + a) % p + p) % p, down = ((2 * P.y) % p + p) % p;
-	int temp = pow(down, p - 2);
-	return ((up*temp) % p + p) % p;
+	long int up = ((3 * P.x * P.x + a) % p + p) % p, down = ((2 * P.y) % p + p) % p;//分子、分母
+	
+	//将分子和分母化为最简
+	long int gcd_value = gcd(up, down);
+	up = int(up / gcd_value);
+	down = int(down / gcd_value);
+
+	//求分母的逆元
+	long int inverse_value = get_inverse_element(down, p);
+	long long int lamda = (up*inverse_value);//求得lamda
+
+	return lamda;
 }
 
 
@@ -85,34 +120,30 @@ point ECurve::PplusQ(point P , point Q )
 {
 	point temp;
 
-	if ((P.x == 0 && P.y == 0) || (Q.x == 0 && Q.y == 0))   //P为O or Q为O  情况
+	if ((P.x == 0 && P.y == 0) || (Q.x == 0 && Q.y == 0))   //P为O or Q为O  情况，返回非零那一个
 		if (P.x == 0)
 			temp.x = Q.x, temp.y = Q.y;
 		else
 			temp.x = P.x, temp.y = P.y;
+	else if (P.x == Q.x&&P.y == p - Q.y)					//P为Q的加法逆元，相加为无穷远点(0,0)
+	{
+		temp.x = 0;
+		temp.y = 0;
+		return temp;
+	}
 	else
-	{												//P，Q都不为O时
-		int lamda;
-		if (P.x == Q.x && P.y == Q.y)					//P and Q 相等情况
+	{												//P，Q都不为O时且非加法逆元
+		long long int lamda;
+		if (P.x == Q.x && P.y == Q.y)				//P and Q 相等情况
 			lamda = equalLamda(P);
 		else										//P and Q 不相等
 			lamda = unEqLamda(P, Q);
-		temp.x = ((lamda * lamda - P.x - Q.x) % p + p) % p;
+
+		temp.x = ((lamda * lamda - P.x - Q.x) % p +p) % p;
 		temp.y = ((lamda * (P.x - temp.x) - P.y) % p + p) % p;
+
 	}
-	
-	bool isO = true;									//P+Q是否是无穷远点
-	int n;
-	for (n = 0; n < vx.size(); n++)
-	{
-		if ((temp.x == (vx[n])) && (temp.y == (vy[n])))	//若当前x,y值存在于循环群列表中(该表不包含0，0)，则不是无穷远点
-		{
-			isO = false;
-			break;
-		}
-	}
-	if(isO)
-		temp.x = 0, temp.y = 0;
+
 	return temp;
 }
 
@@ -134,9 +165,10 @@ point ECurve::kP(int k, point P)
 	return temp;
 }
 
-int ECurve::getOrd(point P)
+int ECurve::getOrd(point P)        //求元素的阶
 {
 	int n = 1;
+
 	point temp = P;
 	while (1)
 	{
@@ -148,36 +180,44 @@ int ECurve::getOrd(point P)
 	return n;
 }
 
-
-
-
-vector<int> ECurve::EncodeMsg(int key, point P, point A_Q, point B_Q, string message)
+//对明文加密
+vector<int> ECurve::EncodeMsg(int A_key, point P, point B_Q, string message)
 {
-	vector<int> secretMsg;
-	int num;
-	point temp;
-	temp = kP(key, B_Q);
-	if (temp.x == 0)
-		temp = PplusQ(P, temp);
-	for (int i = 0; i < message.length(); i++)
+	vector<int> secretMsg;    //密文
+	
+	point C1;//A的公钥
+	C1 = kP(A_key, P);
+	secretMsg.push_back(C1.x);
+	secretMsg.push_back(C1.y);
+	
+	point kQ;
+	kQ = kP(A_key, B_Q);
+
+	unsigned long int C2;
+	for (int i = 0; i < message.length(); i++)		//对逐个字符编码加密
 	{
-		num = (int)message[i] * temp.x;
-		secretMsg.push_back(num);
+		C2 = (int)message[i] + kQ.x;
+		secretMsg.push_back(C2);
 	}
 	return secretMsg;
 }
-//
-string ECurve::DcodeMsg(int key, point P, point A_Q, point B_Q, vector<int> secret)
+
+//对密文解密
+string ECurve::DcodeMsg(int B_key, vector<int> secret)
 {
-	string Dmessage="";
-	point temp;
+	string Dmessage="";        //明文
+
+	point C1;
+	C1.x = secret[0];
+	C1.y = secret[1];
+
+	point xC1;
+	xC1 = kP(B_key, C1);
+
 	int num;
-	temp = kP(key, A_Q);
-	if (temp.x == 0)
-		temp = PplusQ(P, temp);
-	for (int i = 0; i < secret.size(); i++)
+	for (int i = 2; i < secret.size(); i++)          //对逐个数字解密编码
 	{
-		num = secret[i] / temp.x;
+		num = secret[i] - xC1.x;
 		Dmessage += (char)num;
 	}
 	return Dmessage;
--- main.cpp
+++ main.cpp
@@ -1,43 +1,28 @@
 #include"ECurve.h"
 
-
-
 namespace myspace
 {
 	void test_one()
 	{
-		/*int p, a, b;
-		while (1)
-		{
-			cout << "输入p(素数):";
-			cin >> p;
-			cout << "输入a:";
-			cin >> a;
-			cout << "输入b:";
-			cin >> b;
-			if ((4 * (a*a*a) + 27 * (b*b)) % p == 0)
-				cout << "选取的椭圆曲线不能用于加密，请重新选择\n";
-			else
-				break;
-		}*/
-		int p = 11, a = 1, b = 6;
-		//int p = 2833, a = 1, b = 1;
+		
+		int p = 2833, a = 1, b = 1;
+
 		ECurve C(p, a, b);
 		C.showEpab();
-
-		cout << "在以上循环群中选出一个点P作为生成元:";
 		point P;
-		cin >> P.x >> P.y;
-		//P.x = 289;
-		//P.y = 1897;
-		//int n;
-		//n = C.getOrd(P);
-		//cout << "生成元P的阶为：" << n<<endl;
+		P.x = 1341;
+		P.y = 854;
+		
+		cout << "在以上循环群中选出一个点P(" << P.x << "," << P.y << ")" << endl;
+
+		int n = C.getOrd(P);
+		cout << "生成元P(" << P.x << "," << P.y << ")的阶为：" << n << endl;
+
 
 		int B_private_key;
 		cout << "输入B的私钥:";
 		cin >> B_private_key;
-		point B_Q;
+		point B_Q;       //B的公钥
 		B_Q = C.kP(B_private_key, P);
 		cout <<"由生成元P("<<P.x<<","<<P.y<<"),私钥:"<< B_private_key <<"，生成B的公钥Q("<<B_Q.x<<","<<B_Q.y<<")"<<endl;
 		
@@ -46,15 +31,14 @@ namespace myspace
 		cout << "输入A的私钥：";
 		int A_private_key;
 		cin >> A_private_key;
-		point A_Q;//A的公钥
-		A_Q = C.kP(A_private_key, P);
+		
 
 		cout << "输入明文:";
 		string A_Msg;
 		cin >> A_Msg;
 
-		vector<int> A_EncodeMsg;//明文加密
-		A_EncodeMsg = C.EncodeMsg(A_private_key, P, A_Q, B_Q, A_Msg);
+		vector<int> A_EncodeMsg;//明文加密-密文
+		A_EncodeMsg = C.EncodeMsg(A_private_key, P, B_Q, A_Msg);
 		cout << "A加密后将：";
 		for (int i = 0; i < A_EncodeMsg.size(); i++)
 		{
@@ -64,44 +48,92 @@ namespace myspace
 
 
 		cout << endl << "===========B解密==========="<< endl;
+		string B_DecodeMsg;//密文解密-明文
 		int mykey;
 		while (1)
 		{
 			cout << "输入B的私钥：";
-			cin>>mykey;
-			string B_DecodeMsg;
-			B_DecodeMsg = C.DcodeMsg(mykey, P, A_Q, B_Q, A_EncodeMsg);
-			cout << "B解密后：";
+			cin >> mykey;
+			B_DecodeMsg = C.DcodeMsg(mykey, A_EncodeMsg);//用私钥解密
+			cout << "B解密得到明文：";
 			cout << B_DecodeMsg;
+			cout << endl;
 		}
 		
-		
-		
+	}
 
-	}
-	
-	
-	
-	
-	//
-	void test_four()
+	void test_two()   //求元素的阶
 	{
-		string str= "abc";
-		string str2;
-		str2 += 32;
-		int num = 3;
-		for (int i = 0; i < str2.length(); i++)
+		int p = 11, a = 1, b = 6;
+
+		ECurve C(p, a, b);
+		C.showEpab();
+		point P;
+		cout << "=========求元素的阶=========" << endl;
+		while (1)
 		{
-			cout << (int)(str2[i]*num) << "-";
+			cout << "在以上循环群中选出一个点P:";
+			cin >> P.x >> P.y;
+			int n = C.getOrd(P);
+			cout << "生成元P(" << P.x << "," << P.y << ")的阶为：" << n << endl;
+		}
+		
+	}
+
+	void test_three() //求P+Q的结果
+	{
+		int p = 11, a = 1, b = 6;
+
+		ECurve C(p, a, b);
+		C.showEpab();
+		point P;
+		P.x = 2;
+		P.y = 7;
+		cout << "=========求P+Q=========" << endl;
+		while (1)
+		{
+			point Q;
+			cout << "在以上循环群中选出一个点Q:";
+			cin >> Q.x >> Q.y;
+
+			point T;
+			T = C.PplusQ(P, Q);
+			cout << "T: " << T.x << "," << T.y << endl;
+		}
+		
+	}
+
+	void test_four()//求kP
+	{
+		int p = 2833, a = 1, b = 1;
+
+		ECurve C(p, a, b);
+		C.showEpab();
+		point P;
+		P.x = 1341;
+		P.y = 854;
+		cout << "=========求kP=========" << endl;
+		while (1)
+		{
+			int k;
+			cout << "输入k:";
+			cin >> k;
+			point temp;
+			temp = C.kP(k, P);
+			cout<< "temp: " << temp.x << "," << temp.y << endl;
 		}
 	}
+
+	
 }
 	
 
 int main()
 {
-	myspace::test_one();
+	myspace::test_one();       //加密解密
+	//myspace::test_two();       //求元素的阶
+	//myspace::test_three();     //求P+Q的结果
+	//myspace::test_four();      //求kP
 
-	//myspace::test_four();
 	return 0;
 }
